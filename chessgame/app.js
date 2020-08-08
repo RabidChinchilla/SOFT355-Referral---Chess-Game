fdd5d1a2c48e133a8c7f5fd5c0f293e7424f54cd
Ben Gearing <33025242+RabidChinchilla@users.noreply.github.com>
2020-08-08 18:33:02 +0100

    limited the number of connections to the game

--- chessgame/app.js
+++ chessgame/app.js
@@ -1,7 +1,7 @@
 //server file
 var express = require('express');
 var app = express();
-app.use(express.static('public')); 
+app.use(express.static('public'));
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
@@ -15,14 +15,19 @@ http.listen(port, function() {
 });
 
 io.on('connection', function(socket) {
+  if (io.engine.clientsCount > 2)
+  {
+    socket.emit('err', { message: 'reach the limit of connections' })
+    socket.disconnect()
+    console.log('Disconnected...')
+    return
+  }
+  else{
     console.log('new connection');
-});
-
-io.on('connection', function(socket) {
-    console.log('new connection');
+  }
 
     // Called when the client calls socket.emit('move')
     socket.on('move', function(msg) {
-       socket.broadcast.emit('move', msg); 
+       socket.broadcast.emit('move', msg);
     });
 });
